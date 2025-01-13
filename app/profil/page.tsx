783097413f9fa99fae50b119afbdb476381c6705
ziiapaya <ramzysyahputra32@gmail.com>
2025-01-13 12:08:31 +0700

    ramzy:menambahkan profile dan css_modules

---
+++ app/profil/page.tsx
@@ -0,0 +1,9 @@
+// import "style.module.css"
+import style from '../css_modules/style.module.css'
+import React from 'react'
+
+export default function profilpage() {
+  return (
+    <div className={style["text-content"]} >INI HALAMAN PROFILE</div>
+  )
+}
