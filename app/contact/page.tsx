0651f8c47a470a7272245657f0aab8a23d2ea0c9
ziiapayaa <yaakali33@gmail.com>
2025-01-15 00:27:40 +0700

    Ramzy: Menambahkan code ini yang berfungsi untuk menampilkan contact yang terhubung ke perusahaan

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+
+export default function AboutPage() {
+    return (
+      <div className="text-center py-8">
+        <h2 className="text-3xl font-bold text-gray-800">Contact</h2>
+        <p className="text-gray-600 mt-4">
+          kontak perusahaan 
+        </p>
+      </div>
+    );
+  }
