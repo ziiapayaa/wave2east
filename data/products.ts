02c41dc77ac47edbf8ac4b08a687ddacdb0f846f
fariz323232 <achalfarizialghazali@gmail.com>
2025-01-15 02:38:53 +0700

    Fariz: Mengupdate id.

--- data/products.ts
+++ data/products.ts
@@ -56,7 +56,7 @@ export const products = [
     description: 'Experience premium quality and comfort.',
   },
   {
-    id: 5,
+    id: 9,
     name: 'TSHIRT EMELINA BLACK',
     price: 200.00,
     image: '/images/product5.jpg',
