ce91eb5b644055ebecdf0ff634c3deb3423eb828
fariz323232 <achalfarizialghazali@gmail.com>
2025-01-14 19:03:42 +0700

    Fariz: Mengganti text menjadi  Our Latest Products

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div>
       <section className="text-center py-8">
-        <h2 className="text-3xl font-bold text-gray-800">Our Latest T-Shirts</h2>
+        <h2 className="text-3xl font-bold text-gray-800">Our Latest Products</h2>
         <p className="text-gray-600 mt-2">Find the perfect T-shirt for your style and comfort.</p>
       </section>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
