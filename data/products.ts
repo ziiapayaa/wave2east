61fd3a11cc9bd9f3814477a4412aa9da3e85b2a3
fariz323232 <achalfarizialghazali@gmail.com>
2025-01-15 01:36:10 +0700

    Fariz: Mengubah nama product

--- data/products.ts
+++ data/products.ts
@@ -1,7 +1,7 @@
 export const products = [
   {
     id: 1,
-    name: 'Classic T-Shirt',
+    name: 'TSHIRT ISVERRA WHITE',
     price: 19.99,
     image: '/images/tshirt1.jpg',
     description: 'A timeless classic for everyday wear.',
