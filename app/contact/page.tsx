d89ec3eeb8d213a81bd9dacd14b025dbd50f1706
ziiapayaa <yaakali33@gmail.com>
2025-01-18 21:51:30 +0700

    Ramzy: Menambahkan elemen ini di menu contact yang berfungsi sebagai wadah yang memenuhi layar dan menampilkan kontennya di tengah secara vertikal dan horizontal dengan tampilan bersih dan latar belakang putih.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 export default function AboutPage() {
     return (
-      <div className="text-center py-8">
+      <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
         <h2 className="text-3xl font-bold text-gray-800">Contact</h2>
         <p className="text-gray-600 mt-4">
           kontak perusahaan 
