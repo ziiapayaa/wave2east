6423f6f122c457e5c3eae123d7450606803ca975
ziiapayaa <yaakali33@gmail.com>
2025-01-18 21:39:30 +0700

    Ramzy: Menambahkan text ini di dalam about agar menunjang penjelasan yang lebih detail lagi.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,16 @@ export default function AboutPage() {
         <div className="absolute top-1/2 transform -translate-y-1/2 text-center p-6 bg-white shadow-md rounded-lg max-w-3xl"></div>
         <h2 className="text-3xl font-bold text-gray-800">About Us</h2>
         <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify">
-          We are a company dedicated to bringing you the best T-shirt designs for every occasion. 
-          Our mission is to combine comfort and style in every product we offer.
+        Founded in 2013 by Mohan Hazian, Thanksinsomnia is a local clothing brand that focuses on simple and iconic designs, 
+        combining graffiti artwork with a touch of street style. We offer a variety of products ranging from clothing, 
+        accessories, to other unique items that reflect creativity and innovation.
+        Since the beginning, we have been committed to presenting exclusive products with limited stock, giving pride 
+        to our customers. Collaboration with various brands and entities, both local and international, is part of 
+        our strategy to continue to grow and bring something new to the fashion community.
+        Apart from focusing on products, we are also active in social activities. In 2019, together with other brands, 
+        we donated sales proceeds to orphans as part of our corporate social responsibility.
+        By utilizing social media effectively, we have built a solid community and continue to innovate in every product we offer. 
+        Thank you for being part of our journey.
         </p>
       </div>
     );
