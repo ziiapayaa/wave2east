6918812b0152e329ac3ac4e5e3b18b690c151dab
ziiapayaa <yaakali33@gmail.com>
2025-01-18 22:47:24 +0700

    Ramzy: Merapihkan code agar lebih terstruktur.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,32 +12,12 @@ export default function ContactPage() {
 
           {/* Kontak dengan Flexbox */}
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify">
-            <span className="inline-block w-48">📍 Address</span> : 
-            <a href="https://www.google.com/maps?q=Jl+Angsana+Raya+Blok+A+6+Sektor+1.1+BSD+City,+South+Tangerang+15318"
-            target="_blank" rel="noopener noreferrer" className="text-blue-500"> Jl angsana raya blok A 6 sektor 1.1 BSD city</a><br />
-
-            
-            <span className="inline-block w-48">📧 Email</span> : 
-            <a href="mailto:info@thanksinsomnia.net?subject=Inquiry%20About%20Product"
-            target="_blank" rel="noopener noreferrer" className="text-blue-500">info@thanksinsomnia.net</a> <br />
-
-            <span className="inline-block w-48">📞 Call/WhatsApp</span> : 
-            <a href="https://wa.me/+6287880620990" 
-            target="_blank" rel="noopener noreferrer" className="text-blue-500">087880620990</a> <br />
-           
-           
-           
-            <span className="inline-block w-48">🌐 Website</span> :
-            <a href="https://thanksinsomniastore.com/" 
-             target="_blank" rel="noopener noreferrer" className="text-blue-500">thanksinsomniastore.com</a> <br />
-           
-           
-           
-            <span className="inline-block w-48">📸 Instagram</span> :
-            <a href="https://instagram.com/thanksinsomnia" 
-             target="_blank" rel="noopener noreferrer" className="text-blue-500">thxnsmncatalog</a>
+            <span className="inline-block w-48">📍 Address</span>: <a href="https://www.google.com/maps?q=Jl+Angsana+Raya+Blok+A+6+Sektor+1.1+BSD+City,+South+Tangerang+15318" target="_blank" rel="noopener noreferrer" className="text-blue-500">Jl angsana raya blok A 6 sektor 1.1 BSD city</a> <br />
+            <span className="inline-block w-48">📧 Email</span>: <a href="mailto:info@thanksinsomnia.net?subject=Inquiry%20About%20Product" target="_blank" rel="noopener noreferrer" className="text-blue-500">info@thanksinsomnia.net</a> <br />
+            <span className="inline-block w-48">📞 Call/WhatsApp</span>: <a href="https://wa.me/+6287880620990" target="_blank" rel="noopener noreferrer" className="text-blue-500">087880620990</a> <br />
+            <span className="inline-block w-48">🌐 Website</span>: <a href="https://thanksinsomniastore.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500">thanksinsomniastore.com</a> <br />
+            <span className="inline-block w-48">📸 Instagram</span>: <a href="https://instagram.com/thanksinsomnia" target="_blank" rel="noopener noreferrer" className="text-blue-500">thxnsmncatalog</a>
           </p>
-
           <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify">
           For a faster response, you can also contact us via Direct Message on Instagram or via our WhatsApp.
           </p>
