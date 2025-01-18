da705c8325ba3d8a859ba278afec22a6ef3c33d8
ziiapayaa <yaakali33@gmail.com>
2025-01-18 21:54:57 +0700

    Ramzy: Menambahkan elemen ini di menu contact agar kotak konten yang berada di tengah layar secara vertikal, memiliki tampilan rapi dengan latar putih, bayangan, sudut melengkung, dan teks rata tengah.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 export default function AboutPage() {
     return (
       <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
+        <div className="absolute top-1/2 transform -translate-y-1/2 text-center p-6 bg-white shadow-md rounded-lg max-w-3xl"></div>
         <h2 className="text-3xl font-bold text-gray-800">Contact</h2>
         <p className="text-gray-600 mt-4">
           kontak perusahaan 
