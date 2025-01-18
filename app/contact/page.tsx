45afab98280a92b7aec8c5b2fc8221e4b41a4ebd
ziiapayaa <yaakali33@gmail.com>
2025-01-18 22:04:01 +0700

    Ramzy: Menambahkan elemen ini di menu contact agar paragraf dengan teks informatif yang nyaman dibaca, memiliki ukuran besar, warna abu-abu gelap, jarak antar baris yang cukup, dan diratakan kiri-kanan.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,10 +4,13 @@ export default function AboutPage() {
     return (
       <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
         <div className="absolute top-1/2 transform -translate-y-1/2 text-center p-6 bg-white shadow-md rounded-lg max-w-3xl"></div>
-        <h2 className="text-3xl font-bold text-gray-800">Contact</h2>
-        <p className="text-gray-600 mt-4">
+        <h2 className="text-3xl font-extrabold text-gray-800">Contact</h2>
+        <p className="text-gray-700 mt-4 text-lg leading-relaxed text-justify">
+            We are always ready to help you! If you have questions regarding products, orders, or cooperation, don't hesitate to contact us 
+            using the following information:
+          </p>
           kontak perusahaan 
-        </p>
+       
       </div>
     );
   }
